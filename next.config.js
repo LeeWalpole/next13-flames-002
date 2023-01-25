cf1fbc310f51437fe46a9b27db440c39f66a5ea7
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-26 00:07:00 +0700

    PWA Attempt

--- next.config.js
+++ next.config.js
@@ -8,4 +8,15 @@ const nextConfig = {
     unoptimized:true
   }
 }
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
+
 module.exports = nextConfig
