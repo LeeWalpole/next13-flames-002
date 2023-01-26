506086fadcfc6f38e2c2253cd2845fc2aff57ac9
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-01-27 04:02:55 +0700

    Removed content.js

--- components/FeedGallery.jsx
+++
@@ -1,8 +0,0 @@
-import Image from 'next/image'
-export default function ProfileGallery ({ grid_desktop, gallery_alt }) {
-    return (
-      <figure>
-             <Image src={image.gallery_image_url} alt={image.gallery_image_alt} width={image.gallery_image_width} height={image.gallery_image_height}   />
-      </figure>
-    );
-  }
