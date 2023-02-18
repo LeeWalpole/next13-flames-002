b0342e024071ed1f46eb77d0a5fbbb178bdd71b5
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-02-19 02:45:10 +0700

    Remove 404 timeout

--- pages/404.js
+++ pages/404.js
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router'
 const NotFound = () => {
   const router = useRouter()
 
+  /*
   useEffect(() => {
     setTimeout(() => {
       // router.go(-1)
@@ -12,6 +13,7 @@ const NotFound = () => {
       router.push('/')
     }, 3000)
   }, )
+  */
 
   return (
     <div className="not-found">
