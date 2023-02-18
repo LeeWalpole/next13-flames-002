c21514cc1d36cfb82d861ad218046f1e86016c5e
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-02-19 03:33:19 +0700

    Added Refresh Button

--- components/NavLeft.jsx
+++ components/NavLeft.jsx
@@ -17,11 +17,26 @@ export default function NavLeft () {
 <div className={styles.navLeft}>
     <nav className={styles.sticky}>
 
+
         <Link href="/">
           <Image className={styles.logo} src={flamesIcon} height="50" width="50" alt="Flames" />
         </Link>
 
         <ul className={styles.menu}>
+
+
+
+
+<li>
+            <a href="https://flames-agency.vercel.app/">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg>
+            Refresh
+            </a>
+          </li>
+
+
           <li>
             <Link href="/">
             <span>
