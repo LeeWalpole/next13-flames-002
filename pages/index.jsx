a91f82b6e3ce7e10135dfb67b486bdf9d65d666b
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-02-20 12:38:15 +0700

    Loader and SWR on home

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,39 +2,59 @@ import styles from '@/styles/Feed.module.css'
 import Carousel from '../components/Carousel'
 import FeedHeader from '../components/FeedHeader'
 import FeedFooter from '../components/FeedFooter'
+import useSWR from 'swr'
 
-// getServerSideProps
-// getStaticProps
-export const getStaticProps = async () => {  
-  //const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const res = await fetch('https://www.thaiflames.app/backend/wp-json/data/v1/profiles?grab=99', {
-    cache: "no-cache"
-  });
-  const data = await res.json();
-  return {
-    props: { profiles: data } 
+const fetcher = (url) => fetch(url, { cache: "no-cache" }).then(res => res.json());
+
+// function useProfiles() {
+//   const { data, error } = useSWR('https://www.thaiflames.app/backend/wp-json/data/v1/profiles?grab=99', fetcher);
+//   return {
+//     profiles: data,
+//     isLoading: !error && !data,
+//     isError: error
+//   };
+// }
+
+function useProfiles() {
+    const { data, error } = useSWR('https://www.thaiflames.app/backend/wp-json/data/v1/profiles?grab=99', fetcher, {
+      refreshInterval: 5000 // poll every 5 seconds
+    });
+    return {
+      profiles: data,
+      isLoading: !error && !data,
+      isError: error
+    };
   }
-}
+  
 
-const Profiles = ({ profiles }) => {
+const Profiles = () => {
+  const { profiles, isLoading, isError } = useProfiles();
+
+  if (isLoading) {
+    return (
+      <div class="loading-dots">
+        <div class="loading-dots--dot"></div>
+        <div class="loading-dots--dot"></div>
+        <div class="loading-dots--dot"></div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div>Error fetching profiles</div>;
+  }
 
   return (
-
-    <>
-
     <div className={styles.feed}>
       {profiles.map(profile => (
         <div key={profile.username} className={styles.feedItem}>
-        <FeedHeader id={profile.id} username={profile.username} display_name={profile.display_name} bio_mini={profile.bio_mini} avatar_url={profile.avatar_url} />
-        <Carousel images={profile.gallery} />
-        <FeedFooter id={profile.id} username={profile.username} display_name={profile.display_name} bio_mini={profile.bio_mini} avatar_url={profile.avatar_url} />
+          <FeedHeader id={profile.id} username={profile.username} display_name={profile.display_name} bio_mini={profile.bio_mini} avatar_url={profile.avatar_url} />
+          <Carousel images={profile.gallery} />
+          <FeedFooter id={profile.id} username={profile.username} display_name={profile.display_name} bio_mini={profile.bio_mini} avatar_url={profile.avatar_url} />
         </div>
       ))}
     </div>
-
-    </>
-
-
   );
-}
+};
+
 export default Profiles;
--- pages/two.jsx
+++ pages/two.jsx
@@ -3,9 +3,11 @@ import Carousel from '../components/Carousel'
 import FeedHeader from '../components/FeedHeader'
 import FeedFooter from '../components/FeedFooter'
 
-export const getServerSideProps = async () => {  
+// getServerSideProps
+// getStaticProps
+export const getStaticProps = async () => {  
   //const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const res = await fetch('https://www.thaiflames.app/backend/wp-json/data/v1/profiles?grab=99?2', {
+  const res = await fetch('https://www.thaiflames.app/backend/wp-json/data/v1/profiles?grab=99', {
     cache: "no-cache"
   });
   const data = await res.json();
@@ -23,7 +25,7 @@ const Profiles = ({ profiles }) => {
     <div className={styles.feed}>
       {profiles.map(profile => (
         <div key={profile.username} className={styles.feedItem}>
-                  <FeedHeader id={profile.id} username={profile.username} display_name={profile.display_name} bio_mini={profile.bio_mini} avatar_url={profile.avatar_url} />
+        <FeedHeader id={profile.id} username={profile.username} display_name={profile.display_name} bio_mini={profile.bio_mini} avatar_url={profile.avatar_url} />
         <Carousel images={profile.gallery} />
         <FeedFooter id={profile.id} username={profile.username} display_name={profile.display_name} bio_mini={profile.bio_mini} avatar_url={profile.avatar_url} />
         </div>
