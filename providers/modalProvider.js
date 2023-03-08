1f6406c281a68730f3963e4bc509dbe08bdbe544
LeeWalpole <40965437+LeeWalpole@users.noreply.github.com>
2023-03-08 10:20:44 +0700

    Dynamic Dots on Carousel

--- providers/modalProvider.js
+++ providers/modalProvider.js
@@ -1,25 +1,25 @@
-import {createContext, useContext, useState} from "react";
+import { createContext, useContext, useState } from "react";
 
 const ModalContext = createContext(null);
 
-export const useModal = () => useContext(ModalContext)
-const ModalProvider = ({children}) => {
-    const [modals, setModals] = useState({});
+export const useModal = () => useContext(ModalContext);
+const ModalProvider = ({ children }) => {
+  const [modals, setModals] = useState({});
 
-    const handleModal = (...ids) => {
-        let updatedModals = {...modals};
-        ids.forEach(id => {
-            updatedModals[id] = !updatedModals[id]
-        });
+  const handleModal = (...ids) => {
+    let updatedModals = { ...modals };
+    ids.forEach((id) => {
+      updatedModals[id] = !updatedModals[id];
+    });
 
-        setModals(updatedModals);
-    }
+    setModals(updatedModals);
+  };
 
-    return (
-        <ModalContext.Provider value={{modals, handleModal}}>
-            {children}
-        </ModalContext.Provider>
-    )
-}
+  return (
+    <ModalContext.Provider value={{ modals, handleModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
 
-export default ModalProvider
+export default ModalProvider;
